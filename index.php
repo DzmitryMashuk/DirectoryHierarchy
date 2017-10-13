1d49ba9624d95c9a9328a8e644ceb77987247b0e
Mashuk Dzmitry <DzmitryMashuk@gmail.com>
2017-10-13 20:20:22 +0300

    Task. Directory hierarchy complited

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+ function printFile(string $file, int $deep, bool $cli ) : void {
+     if ($cli) {
+	echo str_repeat("---", $deep) . $file . "\n";
+    }
+    else {
+        echo str_repeat('---', $deep) . $file . '<br>';
+    }
+    
+}
+
+function isCli(): bool {
+    return defined('STDIN') || (substr(PHP_SAPI, 0, 3) == 'cgi' && getenv('TERM'));
+}
+
+function readCurrentFile(string $currentDir) : array {
+    $odir = opendir($currentDir);
+     while (($file = readdir($odir)) !== FALSE) {
+         $arrayFile[] = $file;
+     }
+     closedir($odir);
+     return $arrayFile;
+}
+
+function scanDirectory(string $currentDir,int $deep, bool $cli) : void
+{    
+    $arrayFile = readCurrentFile($currentDir);
+    for ($i = 0; $i < count($arrayFile); $i++) {
+        if ($arrayFile[$i] != FALSE && $arrayFile[$i] != '.' && $arrayFile[$i] != '..') {
+            printFile($arrayFile[$i],$deep,$cli);
+            if (is_dir($currentDir.DIRECTORY_SEPARATOR.$arrayFile[$i])){
+                $deep ++;
+                scanDirectory($currentDir.DIRECTORY_SEPARATOR.$arrayFile[$i],$deep,$cli);
+                $deep --;
+            }
+        }
+    }
+}
+
+$cli = isCli();
+scanDirectory(__DIR__,0,$cli);
