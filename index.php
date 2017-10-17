0db0df7f8ae617eb2b1074fc9ec9f94596021f1a
Mashuk Dzmitry <DzmitryMashuk@gmail.com>
2017-10-17 10:52:06 +0300

    modified code

--- index.php
+++ index.php
@@ -1,37 +1,52 @@
 <?php declare(strict_types=1);
 
- function printFile(string $file, int $deep, bool $cli ) : void {
-     if ($cli) {
-	echo str_repeat("---", $deep) . $file . "\n";
+ function printFile(string $file, int $deep, bool $cli ) : void
+ {
+    if ($cli) 
+    {
+		echo str_repeat("---", $deep) . $file . "\n";
     }
-    else {
+    else 
+    {
         echo str_repeat('---', $deep) . $file . '<br>';
     }
     
 }
 
-function isCli(): bool {
+function isCli() : bool
+{
     return defined('STDIN') || (substr(PHP_SAPI, 0, 3) == 'cgi' && getenv('TERM'));
 }
 
-function readCurrentFile(string $currentDir) : array {
+function readCurrentFile(string $currentDir) : array
+{
     $odir = opendir($currentDir);
-     while (($file = readdir($odir)) !== FALSE) {
+
+     while (($file = readdir($odir)) !== FALSE) 
+     {
          $arrayFile[] = $file;
      }
+
      closedir($odir);
+
      return $arrayFile;
 }
 
-function scanDirectory(string $currentDir,int $deep, bool $cli) : void
-{    
+function scanDirectory(string $currentDir, int $deep, bool $cli) : void
+{ 
+
     $arrayFile = readCurrentFile($currentDir);
-    for ($i = 0; $i < count($arrayFile); $i++) {
-        if ($arrayFile[$i] != FALSE && $arrayFile[$i] != '.' && $arrayFile[$i] != '..') {
-            printFile($arrayFile[$i],$deep,$cli);
-            if (is_dir($currentDir.DIRECTORY_SEPARATOR.$arrayFile[$i])){
+
+    for ($i = 0; $i < count($arrayFile); $i++)
+    {
+        if ($arrayFile[$i] != FALSE && $arrayFile[$i] != '.' && $arrayFile[$i] != '..') 
+        {
+            printFile($arrayFile[$i], $deep, $cli);
+
+            if (is_dir($currentDir.DIRECTORY_SEPARATOR.$arrayFile[$i]))
+            {
                 $deep ++;
-                scanDirectory($currentDir.DIRECTORY_SEPARATOR.$arrayFile[$i],$deep,$cli);
+                scanDirectory($currentDir.DIRECTORY_SEPARATOR.$arrayFile[$i], $deep, $cli);
                 $deep --;
             }
         }
@@ -39,4 +54,4 @@ function scanDirectory(string $currentDir,int $deep, bool $cli) : void
 }
 
 $cli = isCli();
-scanDirectory(__DIR__,0,$cli);
+scanDirectory(__DIR__, 0, $cli);
